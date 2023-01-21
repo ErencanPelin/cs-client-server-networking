d0c0b80ca7594fde7b9419f44c0c9e12869e6f46
Erencan Pelin <erencan.pelin@gmail.com>
2023-01-21 17:51:47 +1100

    Add more error handling

--- Net/Server.cs
+++ Net/Server.cs
@@ -10,10 +10,12 @@ namespace Client.Net
         private TcpClient client;
         public PacketReader packetReader;
 
-        public event Action onClientConnect;
-        public event Action onMessageReceive;
-        public event Action onClientDisconnect;
-
+        public event Action onClientConnect; //invoked when the client initially joins the server
+        public event Action onMessageReceive; //invoked when this client receives a message from the server
+        public event Action onClientDisconnect; //invoked when the clinet initially leaves the server
+        public event Action onClientFailedToConnect; //invoked when the user fails to join the server
+        public event Action onClientKicked; //invoked when the user is removed from the connection, or the server is shut down
+        public event Action<string> onMessageSendFailed; //invoked when a message fails to reach the server
         public Server()
         {
             client = new TcpClient();
@@ -23,7 +25,16 @@ namespace Client.Net
         {
             if (!client.Connected)
             {
-                client.Connect("127.0.0.1", 5430);
+                try
+                {
+                    client.Connect("127.0.0.1", 5430);
+                }
+                catch (SocketException e)
+                {
+                    onClientFailedToConnect.Invoke();
+                    return; //stop execution to stop other errors from occuring
+                }
+
                 packetReader = new PacketReader(client.GetStream());
 
                 if (!string.IsNullOrEmpty(username))
@@ -44,22 +55,29 @@ namespace Client.Net
             {
                 while(true)
                 {
-                    var opCode = packetReader.ReadByte();
-
-                    switch (opCode)
+                    try
                     {
-                        case 1:
-                            onClientConnect?.Invoke();
-                            break;
-                        case 5: 
-                            onMessageReceive?.Invoke();
-                            break;
-                        case 10:
-                            onClientDisconnect?.Invoke();
-                            break;
-                        default:
-                            Console.WriteLine("");
-                            break;
+                        var opCode = packetReader.ReadByte();
+
+                        switch (opCode)
+                        {
+                            case 1:
+                                onClientConnect?.Invoke();
+                                break;
+                            case 5:
+                                onMessageReceive?.Invoke();
+                                break;
+                            case 10:
+                                onClientDisconnect?.Invoke();
+                                break;
+                            default:
+                                Console.WriteLine("");
+                                break;
+                        }
+                    } catch (System.IO.IOException e)
+                    {
+                        onClientKicked.Invoke();
+                        return;
                     }
                 }
             });
@@ -70,7 +88,15 @@ namespace Client.Net
             var packet = new PacketBuilder();
             packet.SetOpCode(5);
             packet.WriteMessage(message);
-            client.Client.Send(packet.GetPacketBytes());
+            try
+            {
+                client.Client.Send(packet.GetPacketBytes());
+            }
+            catch (SocketException e)
+            {
+                onMessageSendFailed?.Invoke(message);
+                return;
+            }
         }
     }
 }
--- ViewModel/MainViewModel.cs
+++ ViewModel/MainViewModel.cs
@@ -1,6 +1,7 @@
 ﻿using Client.Core;
 using Client.Model;
 using Client.Net;
+using System;
 using System.Collections.ObjectModel;
 using System.ComponentModel;
 using System.Linq;
@@ -39,28 +40,35 @@ namespace Client.ViewModel
 
             server = new Server();
 
-            server.onClientConnect += ClientConnected;
-            server.onMessageReceive += MessageReceived;
-            server.onClientDisconnect += ClientDisconnected;
+            server.onClientConnect += OnClientConnected;
+            server.onMessageReceive += OnMessageReceived;
+            server.onClientDisconnect += OnClientDisconnected;
+            server.onClientFailedToConnect += OnClientFailedToConnect;
+            server.onClientKicked += OnClientKicked;
+            server.onMessageSendFailed += OnMessageSendFailed;
 
             ConnectCommand = new RelayCommand(o => server.ConnectToServer(Username!.Trim().Trim()), o => !string.IsNullOrEmpty(Username));
             SendCommand = new RelayCommand(o => { server.SendMessageToServer(Message.Trim()); Message = ""; }, o => !string.IsNullOrEmpty(Message));
         }
 
-        private void ClientDisconnected()
+        protected void OnPropertyChanged([CallerMemberName] string name = null) =>
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(name));
+
+        #region Events
+        private void OnClientDisconnected()
         {
             var guid = server.packetReader.ReadMessage();
             var user = Users.Where((o) => o.GUID == guid).First();
-            Application.Current.Dispatcher.Invoke(() => { Users.Remove(user); });
+            Application.Current.Dispatcher.Invoke(() => { Users.Remove(user); }); //update user list
         }
 
-        private void MessageReceived()
+        private void OnMessageReceived()
         {
             var msg = server.packetReader.ReadMessage();
-            Application.Current.Dispatcher.Invoke(() => { Messages.Add(msg); });
+            Application.Current.Dispatcher.Invoke(() => { Messages.Add(msg); }); //update message history
         }
 
-        private void ClientConnected()
+        private void OnClientConnected()
         {
             var username = server.packetReader.ReadMessage();
             var guid = server.packetReader.ReadMessage();
@@ -70,11 +78,27 @@ namespace Client.ViewModel
                 GUID = guid,
             };
 
-            if (!Users.Any((o) => o.GUID == user.GUID))
+            if (!Users.Any((o) => o.GUID == user.GUID)) //update user list
                 Application.Current.Dispatcher.Invoke(() => Users.Add(user)); //since we're doing this on a separate thread
         }
 
-        protected void OnPropertyChanged([CallerMemberName] string name = null) =>
-            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(name));
+        private void OnClientFailedToConnect()
+        {
+            Console.WriteLine("Failed to find server or server refused connection");
+            //do anything else here
+        }
+
+        private void OnMessageSendFailed(string message)
+        {
+            Console.WriteLine("Message failed to reach the server, check internet connection");
+            Application.Current.Dispatcher.Invoke(() => { Messages.Add($"[{DateTime.Now}] Message: \"{message}\" failed to send. Check your network settings or the server might be temporarily shut down."); });
+        }
+
+        private void OnClientKicked()
+        {
+            Console.WriteLine("You were kicked from the server or the server was shutdown");
+            //do anything else here
+        }
+        #endregion
     }
 }
